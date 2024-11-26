712b922577e7a232c078cdc34f7ba787b7b07054
jumang4423 <catontheskate4423@gmail.com>
2024-11-27 00:11:56 +0900

    nyan

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
-import "@fontsource/inter";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./pages/index";
+import App from "./pages/Index";
 import Login from "./pages/login/Index";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Profile from "./pages/profile/Index";
