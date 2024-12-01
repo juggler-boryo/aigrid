e8792c1758c7f97234d70ce6828f7d3284642698
jumang4423 <catontheskate4423@gmail.com>
2024-12-01 23:06:48 +0900

    nyan

--- app/src/components/Tamaki/Tamaki.tsx
+++ app/src/components/Tamaki/Tamaki.tsx
@@ -148,6 +148,7 @@ const Tamaki = () => {
             <Button
               variant="outlined"
               color="primary"
+              size="sm"
               onClick={() => navigate("/tamaki/new")}
               startDecorator={<IoMdAdd />}
               sx={{
