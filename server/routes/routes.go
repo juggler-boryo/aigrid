17655a15b8e215219027cecb534e81829c17eb07
jumang4423 <catontheskate4423@gmail.com>
2024-12-06 23:29:48 +0900

    nyan

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -12,6 +12,8 @@ import (
 func SetupRouter() http.Handler {
 	router := mux.NewRouter()
 	router.HandleFunc("/users/{uid}", handlers.GetUserByUIDHandler).Methods("GET")
+	router.HandleFunc("/niwatori", handlers.WakeUpDiscordNotificationHandler).Methods("POST")
+	router.HandleFunc("/neporeon", handlers.SleepDiscordNotificationHandler).Methods("POST")
 	// -- Protected routes
 	protectedRouter := router.PathPrefix("").Subrouter()
 	protectedRouter.Use(middleware.TokenRequired)
