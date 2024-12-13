f84256dfc93a4f5aebd272ddea1a40beef468973
jumang4423 <catontheskate4423@gmail.com>
2024-12-14 06:00:30 +0900

    nyan

--- app/src/pages/profile/uid/Heatmap.tsx
+++ app/src/pages/profile/uid/Heatmap.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CalendarHeatmap from "react-calendar-heatmap";
 import "react-calendar-heatmap/dist/styles.css";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 interface HeatmapProps {
   data: { date: string; count: number }[];
@@ -8,6 +9,7 @@ interface HeatmapProps {
 
 const Heatmap: React.FC<HeatmapProps> = ({ data }) => {
   const today = new Date().toISOString().split("T")[0];
+  const isMobile = useMediaQuery("(max-width:800px)");
 
   // カスタムスタイル関数
   const getColor = (count: number | undefined): string => {
@@ -17,27 +19,42 @@ const Heatmap: React.FC<HeatmapProps> = ({ data }) => {
     return "#196127"; // 濃い緑
   };
 
+  const startDate = isMobile
+    ? new Date(new Date(today).setMonth(new Date(today).getMonth() - 2))
+    : new Date(new Date(today).setFullYear(new Date(today).getFullYear() - 1));
+
   return (
-    <div style={{ width: "100%", marginTop: "20px" }}>
-      <CalendarHeatmap
-        startDate={`${new Date(today).getFullYear()}-01-01`}
-        endDate={today}
-        values={data}
-        classForValue={undefined} // classを無効化
-        showWeekdayLabels={true}
-        tooltipDataAttrs={(value: { date: string; count: number } | undefined) =>
-          value && value.date
-            ? { "data-tip": `${value.date}: ${value.count} 回` }
-            : { "data-tip": "No data" }
-        }
-        // セルのスタイルを動的に適用
-        transformDayElement={(rect, value) => {
-          const fillColor = getColor(value?.count);
-          return React.cloneElement(rect, {
-            style: { fill: fillColor },
-          });
-        }}
-      />
+    <div
+      style={{
+        width: "100%",
+        marginTop: "20px",
+        maxWidth: isMobile ? "300px" : "100%",
+        margin: "0 auto",
+      }}
+    >
+      <div style={{ width: "100%" }}>
+        <CalendarHeatmap
+          startDate={startDate.toISOString().split("T")[0]}
+          endDate={today}
+          values={data}
+          classForValue={undefined} // classを無効化
+          showWeekdayLabels={true}
+          tooltipDataAttrs={(
+            value: { date: string; count: number } | undefined
+          ) =>
+            value && value.date
+              ? { "data-tip": `${value.date}: ${value.count} 回` }
+              : { "data-tip": "No data" }
+          }
+          // セルのスタイルを動的に適用
+          transformDayElement={(rect, value) => {
+            const fillColor = getColor(value?.count);
+            return React.cloneElement(rect, {
+              style: { fill: fillColor },
+            });
+          }}
+        />
+      </div>
     </div>
   );
 };
