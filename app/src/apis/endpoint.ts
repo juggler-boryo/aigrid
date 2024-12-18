7ddd5d6526f824d9cf651a2478850d0760759827
jumang4423 <catontheskate4423@gmail.com>
2024-12-18 19:10:44 +0900

    pro

--- app/src/apis/endpoint.ts
+++ app/src/apis/endpoint.ts
@@ -4,4 +4,5 @@ export const Endpoint =
     : "https://aigrid-731240201745.asia-northeast1.run.app/";
 
 
-export const ToyuEndpoint = "http://192.168.2.127/"
+// proxy server of finyl, connected to toyu
+export const ToyuEndpoint = "https://192.168.2.105:8443"
