f631347fbe61eea76b1cb4c40039a0e5531716de
ho-ryu-ji <ryuta.soc7@gmail.com>
2025-01-04 03:37:57 +0900

    create StayTimeTable

--- app/src/pages/inout/anal/Index.tsx
+++ app/src/pages/inout/anal/Index.tsx
@@ -2,7 +2,7 @@ import { useIdToken } from "react-firebase-hooks/auth";
 import { auth } from "../../../libs/firebase";
 import { useQuery } from "@tanstack/react-query";
 import { getInoutAnalytics } from "../../../apis/inout";
-import { Box, CircularProgress, Card, Typography, Divider } from "@mui/joy";
+import { Box, CircularProgress, Card, Typography, Divider, Table } from "@mui/joy";
 import { Inout } from "../../../types/inout";
 import { PieChart, LineChart } from "@mui/x-charts";
 import { Min2Str } from "../../../libs/min2str";
@@ -109,11 +109,46 @@ const getDailyMinutesByUser = (inoutList: Inout[]) => {
   return userDailyMinutes;
 };
 
-const Index = () => {
+const calculateMonthlyStayTime = (inoutList: any[]) => {
+  const result: { [key: string]: number } = {};
+
+  inoutList.forEach((record) => {
+    const uid = record.uid;
+    const createdAt = new Date(record.created_at);
+    const month = createdAt.getFullYear() - (createdAt.getMonth() + 1);
+
+    if (!result[uid]) result[uid] = 0;
+    if (record.is_in) {
+      const nextRecord = inoutList.find(
+        (r) => r.uid === uid && !r.is_in && new Date(r.created_at) > createdAt
+      );
+      if (nextRecord) {
+        const diffMinutes =
+          (new Date(nextRecord.created_at).getTime() - createdAt.getTime()) /
+          (1000 * 60);
+        result[uid] += diffMinutes;
+      }
+    }
+  });
+
+  // 直近の月のデータのみ取得
+  const currentDate = new Date();
+  const lastMonth =
+    currentDate.getFullYear() +
+    "-" +
+    (currentDate.getMonth() === 0 ? 12 : currentDate.getMonth());
+
+  return Object.entries(result).reduce((acc, [uid, minutes]) => {
+    if (uid.startsWith(lastMonth)) acc[uid] = Math.round(minutes);
+    return acc;
+  }, {} as { [key: string]: number });
+};
+
+const StayTimeTable = () => {
   const [user] = useIdToken(auth);
-  const { data, isFetching } = useQuery({
-    queryKey: ["inoutList", user?.uid],
-    queryFn: async () => {
+  const { data: inoutData, isLoading, error } = useQuery({
+    queryKey: ['inoutAnalytics'],
+    queryFn: async ({ }) => {
       const accessToken = await user?.getIdToken();
       const inoutList = await getInoutAnalytics(accessToken || "");
       const uids = [...new Set(inoutList.map((record) => record.uid))];
@@ -123,47 +158,29 @@ const Index = () => {
       return {
         inoutList,
         userMap,
-      };
+      }
     },
-    enabled: !!user,
+    enabled: !!user
   });
 
-  const { chartData } = useMemo(() => {
-    if (!data?.inoutList) return { chartData: [] };
+  const monthlyStayTime = useMemo(() => {
+    if (!inoutData) return {};
+    return InOutList2EachPersonMinutes(inoutData.inoutList);
+  }, [inoutData]);
 
-    const minutes = InOutList2EachPersonMinutes(data.inoutList);
-    const chart = Object.entries(minutes)
-      .filter(([, mins]) => mins > 0)
-      .map(([uid, mins], index) => ({
-        id: index,
-        value: mins,
-        label: `${data?.userMap[uid]?.username || "Unknown User"} (${Min2Str(
-          mins
-        )})`,
-      }));
+  const tableData = useMemo(() => {
+    if (!inoutData) return [];
 
-    return { chartData: chart };
-  }, [data?.inoutList, data?.userMap]);
+    const totalMinutes = Object.values(monthlyStayTime).reduce((sum, minutes) => sum + minutes, 0);
 
-  const { userSeries, allDates } = useMemo(() => {
-    if (!data?.inoutList) return { userSeries: [], allDates: [] };
-
-    const dailyData = getDailyMinutesByUser(data.inoutList);
-    const series = Object.entries(dailyData)
-      .filter(([, userData]) => userData.length > 0)
-      .map(([uid]) => ({
-        data: dailyData[uid].map((d) => d.minutes),
-        label: data?.userMap[uid]?.username || "Unknown User",
-      }));
-
-    const dates = Object.values(dailyData)
-      .flat()
-      .map((d) => d.date)
-      .filter((date, index, self) => self.indexOf(date) === index)
-      .sort((a, b) => a - b);
-
-    return { userSeries: series, allDates: dates };
-  }, [data?.inoutList, data?.userMap]);
+    return Object.entries(monthlyStayTime)
+      .map(([uid, minutes]) => {
+        const userName = inoutData.userMap[uid]?.username || "Unknown User";
+        const percentage = ((minutes / totalMinutes) * 100).toFixed(2);
+        return { uid, userName, minutes, percentage };
+      })
+      .sort((a, b) => b.minutes - a.minutes);
+  }, [inoutData, monthlyStayTime]);
 
   return (
     <CheckAuth>
@@ -174,72 +191,187 @@ const Index = () => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          justifyContent: "center",
         }}
       >
         <TopBar />
-        <CoolMo>
-          <Card sx={{ width: "85%" }}>
-            {isFetching ? (
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <CircularProgress />
-              </Box>
-            ) : chartData.length > 0 &&
-              userSeries.length > 0 &&
-              allDates.length > 0 ? (
-              <Box gap={1} display="flex" flexDirection="column">
-                <Box>
-                  <PieChart
-                    series={[
-                      {
-                        data: chartData,
-                        highlightScope: {
-                          faded: "global",
-                          highlighted: "item",
-                        },
-                        faded: { innerRadius: 30, additionalRadius: 30 },
-                      },
-                    ]}
-                    width={999}
-                    height={400}
-                    slotProps={{
-                      legend: {
-                        itemGap: 10,
-                      },
-                    }}
-                  />
-                </Box>
-                <Box m={0.5}>
-                  <Divider />
-                </Box>
-                <Box>
-                  {userSeries.length > 0 && allDates.length > 0 && (
-                    <LineChart
-                      series={userSeries}
-                      width={999}
-                      height={400}
-                      xAxis={[
-                        {
-                          data: allDates,
-                          scaleType: "time",
-                        },
-                      ]}
-                      slotProps={{
-                        legend: {
-                          itemGap: 10,
-                        },
-                      }}
-                    />
-                  )}
-                </Box>
-              </Box>
-            ) : (
-              <Typography level="title-md">No data available</Typography>
-            )}
-          </Card>
-        </CoolMo>
+        <Box width={"90%"}>
+          <Divider />
+        </Box>
       </Box>
+      <CoolMo>
+        <Box
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            maxWidth: 1000,
+            minWidth: { xs: "300px", sm: "400px" },
+            mx: "auto",
+            p: 2,
+          }}
+        >
+
+          <Table aria-label="stay time table">
+            <thead>
+              <tr>
+                <th style={{ width: "20%" }}>User</th>
+                <th>Stay Time of Last Month (min)</th>
+                <th>Percentage </th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map(({ uid, userName, minutes, percentage }) => (
+                <tr key={uid}>
+                  <td>{userName}</td>
+                  <td>{minutes} minutes</td>
+                  <td>{percentage} %</td>
+                </tr>
+              )
+              )}
+            </tbody>
+          </Table>
+        </Box>
+      </CoolMo>
     </CheckAuth>
   );
 };
 
-export default Index;
+export default StayTimeTable;
+
+// 円グラフ
+// const Index = () => {
+//   const [user] = useIdToken(auth);
+//   const { data, isFetching } = useQuery({
+//     queryKey: ["inoutList", user?.uid],
+//     queryFn: async () => {
+//       const accessToken = await user?.getIdToken();
+//       const inoutList = await getInoutAnalytics(accessToken || "");
+//       const uids = [...new Set(inoutList.map((record) => record.uid))];
+//       const userPromises = uids.map((uid) => GetUser(uid));
+//       const users = await Promise.all(userPromises);
+//       const userMap = Object.fromEntries(users.map((user) => [user.uid, user]));
+//       return {
+//         inoutList,
+//         userMap,
+//       };
+//     },
+//     enabled: !!user,
+//   });
+
+//   const { chartData } = useMemo(() => {
+//     if (!data?.inoutList) return { chartData: [] };
+
+//     const minutes = InOutList2EachPersonMinutes(data.inoutList);
+//     const chart = Object.entries(minutes)
+//       .filter(([, mins]) => mins > 0)
+//       .map(([uid, mins], index) => ({
+//         id: index,
+//         value: mins,
+//         label: `${data?.userMap[uid]?.username || "Unknown User"} (${Min2Str(
+//           mins
+//         )})`,
+//       }));
+
+//     return { chartData: chart };
+//   }, [data?.inoutList, data?.userMap]);
+
+//   const { userSeries, allDates } = useMemo(() => {
+//     if (!data?.inoutList) return { userSeries: [], allDates: [] };
+
+//     const dailyData = getDailyMinutesByUser(data.inoutList);
+//     const series = Object.entries(dailyData)
+//       .filter(([, userData]) => userData.length > 0)
+//       .map(([uid]) => ({
+//         data: dailyData[uid].map((d) => d.minutes),
+//         label: data?.userMap[uid]?.username || "Unknown User",
+//       }));
+
+//     const dates = Object.values(dailyData)
+//       .flat()
+//       .map((d) => d.date)
+//       .filter((date, index, self) => self.indexOf(date) === index)
+//       .sort((a, b) => a - b);
+
+//     return { userSeries: series, allDates: dates };
+//   }, [data?.inoutList, data?.userMap]);
+
+//   return (
+//     <CheckAuth>
+//       <Box
+//         sx={{
+//           mx: "auto",
+//           gap: 2,
+//           display: "flex",
+//           flexDirection: "column",
+//           alignItems: "center",
+//         }}
+//       >
+//         <TopBar />
+//         <CoolMo>
+//           <Card sx={{ width: "85%" }}>
+//             {isFetching ? (
+//               <Box display="flex" justifyContent="center" alignItems="center">
+//                 <CircularProgress />
+//               </Box>
+//             ) : chartData.length > 0 &&
+//               userSeries.length > 0 &&
+//               allDates.length > 0 ? (
+//               <Box gap={1} display="flex" flexDirection="column">
+//                 <Box>
+//                   <PieChart
+//                     series={[
+//                       {
+//                         data: chartData,
+//                         highlightScope: {
+//                           faded: "global",
+//                           highlighted: "item",
+//                         },
+//                         faded: { innerRadius: 30, additionalRadius: 30 },
+//                       },
+//                     ]}
+//                     width={999}
+//                     height={400}
+//                     slotProps={{
+//                       legend: {
+//                         itemGap: 10,
+//                       },
+//                     }}
+//                   />
+//                 </Box>
+//                 <Box m={0.5}>
+//                   <Divider />
+//                 </Box>
+//                 <Box>
+//                   {userSeries.length > 0 && allDates.length > 0 && (
+//                     <LineChart
+//                       series={userSeries}
+//                       width={999}
+//                       height={400}
+//                       xAxis={[
+//                         {
+//                           data: allDates,
+//                           scaleType: "time",
+//                         },
+//                       ]}
+//                       slotProps={{
+//                         legend: {
+//                           itemGap: 10,
+//                         },
+//                       }}
+//                     />
+//                   )}
+//                 </Box>
+//               </Box>
+//             ) : (
+//               <Typography level="title-md">No data available</Typography>
+//             )}
+//           </Card>
+//         </CoolMo>
+//       </Box>
+//     </CheckAuth>
+//   );
+// };
+
+// export default Index;
